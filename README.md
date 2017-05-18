8ce7921803be65f6d6e6d1b63ad668a27a96a10d
Gil Mor <gilmor99@gmail.com>
2017-05-18 15:12:08 -0700

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Logs
+
+logs.py--implementation of the Logs Analysis Project.
+
+The application connects to the News db and print out answers
+to the following 3 questions -
+    1. What are the most popular three articles of all time?
+    2. Who are the most popular article authors of all time?
+    3. On which days did more than 1% of requests lead to errors?
+
+To run please print - 'python logs.py' in the command line.
+
+Enjoy!
--- logs.py
+++ logs.py
@@ -1,9 +1,8 @@
 """
 logs.py--implementation of the Logs Analysis Project.
 
-The application connects to the News db amd print out answers
-to the follwoing 3 questions -
-
+The application connects to the News db add print out answers
+to the following 3 questions -
     1. What are the most popular three articles of all time?
     2. Who are the most popular article authors of all time?
     3. On which days did more than 1% of requests lead to errors?
